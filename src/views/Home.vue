f0a94b7a59fc63259f7502c1babff5c07feecfb6
Newton Darte <newton_@hotmail.com>
2020-07-22 20:51:12 -0300

    Initial project setup. Added axios and an input with a method to search github users.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input
+      v-model="term"
+      type="search"
+      placeholder="Enter a GitHub User ..."
+      name="search"
+      id="search"
+      @keyup.enter="search"
+    >
+    <div v-if="githubUser">
+      <h4>{{ githubUser.name }}</h4>
+      <img :src="githubUser.avatar_url" :alt="githubUser.name">
+      <p>Public repos: {{ githubUser.public_repos }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    githubUser: null,
+    term: '',
+    timeout: null
+  }),
+  methods: {
+    async getGithubUser() {
+      const response = await axios.get(`https://api.github.com/users/${this.term.trim()}`);
+      this.githubUser = response.data;
+      console.log(response.data);
+    },
+    search() {
+      clearTimeout(this.timeout);
+      this.timeout = null;
+      setTimeout(() => this.getGithubUser(), 2000);
+    }
   }
 }
 </script>
